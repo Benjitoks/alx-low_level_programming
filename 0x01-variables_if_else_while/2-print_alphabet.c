c921cf4efa6659c40be0ff26054b5ca6952e0a67
Benjitoks <tokorbenjamin@gmail.com>
2023-08-14 03:36:49 -0700

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints the last digit of a randomly generated number
+ * whether it is greater than 5, less than 6, or 0
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c)
+			putchar('\n');
+	return (0);
+}
+
